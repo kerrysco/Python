aaa12f6d848e1460b6bf703545cff9426fe96181
Kerry Scott <Kerry Scott>
2017-01-02 16:06:28 -0500

    Python learning exercises.

---
+++ exercises/ex5.py
@@ -0,0 +1,13 @@
+my_name = "Kerry Scott"
+my_age = 60
+my_height = 6 * 12
+my_weight = 270
+my_eyes = 'green'
+my_teeth = 'white'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He is %d pounds heave." % my_weight
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "If I add %d, %d and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
